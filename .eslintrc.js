bf3d5c04754e92595e6268636d81c1ec749bdc57
Lucas Gregory <lgregory12@hotmail.com>
2020-07-30 10:27:03 -0300

    struc: storybook vue.
    add vue eslint config.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2020: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:vue/essential',
+    '@vue/typescript/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'prettier',
+    'import',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  settings: {
+    'import/resolver': {
+        'typescript': {}
+    }
+  }
+}

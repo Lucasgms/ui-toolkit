3f2229f6662e72a13deb710bd28da8ad2ef3dfb6
Lucas Gregory <lgregory12@hotmail.com>
2020-08-04 17:04:16 -0300

    feat: tokens
    add initial doc file

---
+++ .storybook/preview.js
@@ -0,0 +1,5 @@
+import { addParameters } from '@storybook/vue';
+
+// import '../src/styles/global/reset.css';
+import '../config/storybook/storybook.css';
+import '../src/styles/global/variables.css';

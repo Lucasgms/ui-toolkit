f00864c249a0b271c9b12ff5bffaf84e5910fb3e
Lucas Gregory <lgregory12@hotmail.com>
2020-08-06 11:09:44 -0300

    struc: add global css files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,3 @@
-import { addParameters } from '@storybook/vue';
-
-// import '../src/styles/global/reset.css';
+import '../src/styles/global/reset.css';
 import '../src/core/storybook/styles/storybook.css';
-import '../src/styles/global/variables.css';
+import '../src/styles/global/index.css';

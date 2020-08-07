b7b63e14dd2abbc78edcac5e809603931c40984c
Lucas Gregory <lgregory12@hotmail.com>
2020-08-07 17:52:47 -0300

    feat: implement icon component

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <i :class="iconClass">
+    {{ icon }}
+  </i>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Icon extends Vue {
+  @Prop({ default: '' }) icon!: string;
+  @Prop({ default: 'medium' }) size!: number;
+  @Prop({ default: 'default' }) color!: number;
+
+  get iconClass() {
+    return ['material-icons', 'icon', this.size, this.color];
+  }
+}
+</script>

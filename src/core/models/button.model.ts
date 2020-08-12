2577a11b28cdf66ac06d5489ab31538914bd215f
Lucas Gregory <lgregory12@hotmail.com>
2020-08-12 14:22:04 -0300

    feat: button
    remove unused props
    adjust outlined button colors

--- src/core/models/button.model.ts
+++ src/core/models/button.model.ts
@@ -2,13 +2,9 @@ export interface IButton {
   /**  'default' | 'primary' | 'secondary' */
   color?: 'default' | 'primary' | 'secondary'
   /** boolean */
-  shape?: 'round-square' | 'circle'
-  /** boolean */
   disabled?: boolean
   /** href <a> */
   href?: string
-  /** boolean */
-  isLoading?: boolean
   /** 'large' | 'medium' | 'small' */
   size?: 'large' | 'medium' | 'small'
   /** 'filled' | 'outlined' | 'text' */
